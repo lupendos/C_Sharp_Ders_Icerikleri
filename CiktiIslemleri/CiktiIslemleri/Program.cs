39ac46917674014e9655b1155b3b41d6797db9c4
Fırat Orak <firatorakk@gmail.com>
2024-10-17 13:32:56 +0300

    Add files via upload

---
+++ CiktiIslemleri/CiktiIslemleri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiktiIslemleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("fırat orak"); //Konsola yazı yazar.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.ReadKey(); //Konsolu Ekranda tutar.
+        }
+    }
+}
