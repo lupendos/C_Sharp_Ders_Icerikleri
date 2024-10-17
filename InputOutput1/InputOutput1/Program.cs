e0922ce3a8b01ff0fe44a2cadf47efa389388de5
Fırat Orak <firatorakk@gmail.com>
2024-10-17 13:33:35 +0300

    Add files via upload

---
+++ InputOutput1/InputOutput1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //OUTPUT
+            Console.WriteLine("Mert Cem Kılıçaslan");
+            Console.WriteLine(12*13);
+
+
+
+            Console.ReadKey(); //1 Karakter(klavyenin herhangi bir tuşu) kodu tamamlar.
+        }
+    }
+}
