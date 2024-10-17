4246d4837700d80fa1271fcd85599434f50bf073
Fırat Orak <firatorakk@gmail.com>
2024-10-17 13:33:15 +0300

    Add files via upload

---
+++ InputOutput/InputOutput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Ana Fonksiyon
+        {
+            Console.WriteLine("Deneme");
+
+
+
+        }
+    }
+}
